336d0caa553edf48b73ffc51e1cc325c93494272
John Collins <john.collins@braincorp.com>
2021-10-04 14:59:08 -0700

    New scad-paths

--- src/main/foam_generator/core.clj
+++ src/main/foam_generator/core.clj
@@ -6,120 +6,170 @@
    [scad-clj.scad :as s]
    [scad-paths.core :as paths
     :refer [context forward hull left right up down roll backward defmodel no-op translate
-            lookup-transform rotate transform]]))
+            model branch segment set
+            ctx spin lookup-transform rotate transform]]))
 
-(def ctx
+(def cx
   {:fn 10 :curve-radius 10})
 
 (def pi Math/PI)
 (def hpi (/ Math/PI 2))
 
 (defmodel air-hose-plug
-  (assoc ctx :fn 10)
-  (let [c1o (m/circle p/plug-or)
-        c1i (m/circle p/plug-ir)
+  :fn 10
+  (segment
+   (let [c1o (m/circle p/plug-or)
+         c1i (m/circle p/plug-ir)
 
-        c2o (m/circle p/plug-third-segment-radius)
-        c2i (m/circle (- p/plug-third-segment-radius 3/2))
+         c2o (m/circle p/plug-third-segment-radius)
+         c2i (m/circle (- p/plug-third-segment-radius 3/2))
 
-        bulge-circle (m/circle p/plug-bulge-or)]
-    [[(context :shape c2o) (context :shape c2i)]
-     (forward :length p/plug-third-segment-length)
-     [(context :shape c1o) (context :shape c1i)]
-     (forward :length p/plug-second-hull-length)
-     (hull)
-     (forward :length p/plug-second-segment-length)
-     (forward :length p/plug-bulge-hull-length)
-     [(context :shape bulge-circle)]
-     (forward :length p/plug-bulge-segment-length)
-     (hull)
-     [(context :shape c1o)]
-     (forward :length p/plug-bulge-hull-length)
-     (hull)
-     (forward :length p/plug-first-segment-length)]))
+         bulge-circle (m/circle p/plug-bulge-or)]
+     [(model :name :body :shape c2o :mask? false)
+      (model :name :mask :shape c2i :mask? true)
 
+      (backward :to [:mask] :length 0.01)
+      (forward :to [:mask] :length 0.01)
+      (forward :length p/plug-third-segment-length)
+
+      (set :shape c1o :to [:body])
+      (set :shape c1i :to [:mask])
+
+      (forward :length p/plug-second-hull-length)
+      (hull)
+      (forward :length p/plug-second-segment-length)
+      (forward :length p/plug-bulge-hull-length)
+
+      (set :shape bulge-circle :to [:body])
+      (forward :length p/plug-bulge-segment-length)
+      (hull)
+      (set :shape c1o :to [:body])
+      (forward :length p/plug-bulge-hull-length)
+      (hull)
+      (forward :length p/plug-first-segment-length :to [:body])
+      (forward :length (+ 0.01 p/plug-first-segment-length) :to [:mask])])))
 
 (defmodel tube-connector
-  (assoc ctx :fn 10)
-  (let [outer-circle-medium (m/circle p/tubing-ir)
-        outer-circle-small (m/circle (- p/tubing-ir 1/2))
-        inner-circle (m/circle (- p/tubing-ir 1/2 3/2))]
-    (into []
-          cat
-          (for [x (range 3)]
-            [[(context :shape outer-circle-medium) (context :shape inner-circle)]
-             (forward :length 1)
-             [(context :shape outer-circle-small)]
-             (forward :length 4)
-             (hull)]))))
+  :fn 100
+  (segment
+   (let [outer-circle-medium (m/circle p/tubing-ir)
+         outer-circle-small (m/circle (- p/tubing-ir 1/2))
+         inner-circle (m/circle (- p/tubing-ir 1/2 3/2))]
+     (into [(model :name :body :shape outer-circle-medium :mask? false)
+            (model :name :mask :shape inner-circle :mask? true)]
+           cat
+           (for [_ (range 3)]
+             [(set :shape outer-circle-medium :to [:body])
+              (set :shape inner-circle :to [:mask])
 
-(defmodel intake-plate
-  ctx
-  (let [c (m/circle p/intake-or)]
-    [[(context :shape c)]
-     (forward :length p/intake-plate-thickness)
-     [:segment
-      (vec
-       (for [x (range 3)]
-         (let [d (* x (/ (* 2 Math/PI) 3))]
-           [:branch [(translate :x (* 10 (Math/cos d)) :y (* 10 (Math/sin d)))
-                     [:segment (if (zero? x) air-hose-plug tube-connector)]]])))]]))
+              (forward :length 1)
+              (set :shape outer-circle-small :to [:body])
+              (forward :length 4)
+              (hull)])))))
 
 (defmodel squeeze-trigger
-  (assoc ctx :curve-radius 40)
-  (let [shape (m/circle 3)]
-    [[(context :shape shape)]
-     (forward :length 10)
-     (left :curve-radius 3)
-     (forward :length 60)
-     (left :curve-radius 3)
-     (forward :length 20)
-     (hull :n-segments 3)
-     (left :curve-radius 3)
-     (roll :angle Math/PI)
-     (left :angle (/ Math/PI 6))
-     [(context :curve-radius 60)]
-     (right :angle (/ Math/PI 6))
-     (left :angle (/ Math/PI 3) :curve-radius 4)]))
+  :fn 10
+  (model :shape (m/circle 3) :name :body :mask? false)
+  (forward :length 10)
+  (left :curve-radius 3)
+  (forward :length 60)
+  (left :curve-radius 3)
+  (forward :length 20)
+  (hull :n-segments 3)
+  (left :curve-radius 3)
+  (roll :angle Math/PI)
+  (left :angle (/ Math/PI 6))
+  (set :curve-radius 60)
+  (right :angle (/ Math/PI 6))
+  (left :angle (/ Math/PI 3) :curve-radius 4))
 
 (defmodel gun-body
-  ctx
-  (let [main-shape (m/minkowski (m/square 29 15)
-                                (m/circle 3))]
-    [[:branch
-      [(rotate :axis [0 1 0] :angle (+ (/ Math/PI 8) Math/PI))
-       [:segment air-hose-plug]]]
-     [(context :shape main-shape :curve-radius 35/2)]
-     (rotate :axis [0 1 0] :angle (/ Math/PI 8))
-     #_(rotate :axis [0 1 0] :angle (/ Math/PI 6))
-     (forward :length 90)
-     (right :angle (- (/ Math/PI 2) (/ Math/PI 8)))
-     [(context :shape (m/circle 15))]
-     (forward :length 130)
-     (hull)]))
+  :fn 100
+  (model :shape (m/minkowski (m/square 35 15) (m/circle 3))
+         :name :body
+         :curve-radius 41/2)
+
+  (model :shape nil :name :mask :mask? true)
+
+  (branch
+   (rotate :axis [0 1 0] :angle (+ (/ Math/PI 8) Math/PI))
+   (segment air-hose-plug))
+
+  (branch
+   (rotate :axis [0 1 0] :angle (+ (/ Math/PI 8) Math/PI))
+   (translate :x 14 :z 0)
+   (segment tube-connector))
+
+  (branch
+   (rotate :axis [0 1 0] :angle (+ (/ Math/PI 8) Math/PI))
+   (translate :x (- 14) :z 0)
+   (segment tube-connector))
+
+  (rotate :axis [0 1 0] :angle (/ Math/PI 8))
+  (forward :length 90)
+  (right :angle (- (/ Math/PI 2) (/ Math/PI 8)))
+  (set :shape (m/circle 15))
+  (forward :length 130))
+
+(defmodel valve-cap
+  :fn 10
+  (model :shape (m/circle 10) :mask? false :name :body)
+  (model :shape (u/ovol 9 2) :mask? true :name :mask)
+  (forward :length 4 :to [:body])
+  (translate :z 4 :to [:mask])
+  (spin :angle (* 2 pi) :to [:mask]))
 
 (defmodel trigger
-  ctx
-  (let [shape (m/square 8 6)]
-    [[(context :shape shape :curve-radius 15)]
-     (rotate :axis [0 1 0] :angle (/ Math/PI 8))
-     (forward :length 10 :gap false)
-     [:branch
-      [(right :angle (/ Math/PI 2) :curve-radius 4 :gap false)
-       (forward :length 10)]]
-     (forward :length 60)
-     (right :angle (- (/ Math/PI 2) (/ Math/PI 8))
-            :curve-radius 4)
-     (forward :length (- 15 3/2))
-     (right :angle (* 2 Math/PI) :curve-radius 4)
-     (right :curve-radius 4 :angle (/ Math/PI 2.3))
-     (right :angle (/ Math/PI 8) :curve-radius 40)
-     (left :angle (/ Math/PI 3.8) :curve-radius 60)]))
+  :fn 10
+  (model :shape (m/square 8 6) :curve-radius 15 :mask? false :name :body)
+  (rotate :axis [0 1 0] :angle (/ Math/PI 8))
+  (forward :length 10 :gap false)
+  (branch
+   (right :angle (/ Math/PI 2) :curve-radius 4 :gap false)
+   (forward :length 10))
 
-(m/union trigger gun-body)
+  (forward :length 60)
+  (right :angle (- (/ Math/PI 2) (/ Math/PI 8))
+         :curve-radius 4)
+  (forward :length (- 15 3/2))
+  (right :angle (* 2 Math/PI) :curve-radius 4)
+  (right :curve-radius 4 :angle (/ Math/PI 2.3))
+  (right :angle (/ Math/PI 8) :curve-radius 40)
+  (left :angle (/ Math/PI 3.8) :curve-radius 60)
+  )
+
+(defn cosine-hill [w h n-steps]
+  (m/polygon
+   (for [step (range n-steps)]
+     (let [step step
+           x (- (* step (/ w (dec n-steps))) (/ w 2))
+           y (* (/ h 2) (Math/cos (* step (/ (* 2 Math/PI) (dec n-steps)))))]
+       [x y]))))
+
+(defmodel intake
+  :fn 10
+  (model :shape (m/circle p/plug-third-segment-radius) :mask? false :name :body)
+  (model :shape (m/circle (- p/plug-third-segment-radius 3/2)) :mask? false :name :mask)
+
+  (forward :length 1)
+
+  (set :shape (cosine-hill 12 3 50) :to [:body])
+  (set :shape nil :to [:mask])
+
+  (translate :y 4)
+  (forward :length 10)
+  (hull)
+  (forward :length 20)
+  (branch
+   (forward :length 20))
+  (down :angle Math/PI :curve-radius 3)
+  (forward :length 20)
+  (forward :length 1)
+  (translate :y (- 2) :z 9)
+  (segment tube-connector))
 
 (defmodel spring
-  (assoc ctx :fn 100)
+  (assoc cx :fn 100)
   (let [a (/ pi 8)
         width 10
         n-turns 4
@@ -138,50 +188,3 @@
               (roll :angle pi)]))
      (left :angle a :curve-radius (/ thickness 2))
      (forward :length 10))))
-
-(defn cosine-hill [w h n-steps]
-  (m/polygon
-   (for [step (range n-steps)]
-     (let [step step
-           x (- (* step (/ w (dec n-steps))) (/ w 2))
-           y (* (/ h 2) (Math/cos (* step (/ (* 2 Math/PI) (dec n-steps)))))]
-       [x y]))))
-
-(defmodel intake
-  ctx
-  [#_[:branch
-    [(rotate :axis [0 1 0] :angle pi)
-     [:segment air-hose-plug]]]
-   [(context :shape (m/circle p/plug-third-segment-radius))
-    (context :shape (m/circle (- p/plug-third-segment-radius 3/2)))]
-   (forward :length 1)
-   [(context :shape (cosine-hill 12 3 50)) (context :shape nil)]
-   #_[:branch
-    [(up :curve-radius 5)
-     (down :curve-radius 5)]]
-   (translate :y 4)
-   (forward :length 10)
-   (hull)
-   (forward :length 20)
-   (down :angle Math/PI :curve-radius 3/2)
-   (forward :length 20)
-   (forward :length 1)
-   (translate :y 4 :z -9)
-   [:segment
-    (let [[s1 s2 & segs] (:path-spec (meta tube-connector))]
-      (vec (list* s1 s2 (hull :n-segments 2) nil)))]])
-
-(defmodel tmp
-  ctx
-  [[(context :shape (cosine-hill 12 3 50))
-    (context :shape (cosine-hill 6 3/2 50))]
-   (forward :length 50)])
-
-(defmodel living-spring
-  ctx
-  (let [angle (/ Math/PI 4)
-        r 40
-        shape (m/square 2 10)]
-    [[(context :shape shape)]
-     (rotate :axis [0 1 0] :angle (- (/ angle 2)))
-     (right :curve-radius r :angle angle)]))
